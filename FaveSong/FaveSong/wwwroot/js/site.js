b30ffe5a1211f39fd475f150c1967fe0dc361c25
wilsonkimberley <kimberleyannuk@gmail.com>
2025-11-04 00:18:40 +0000

    FaveSongProject

---
+++ FaveSong/FaveSong/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Fade-in animation
+document.body.style.opacity = 0;
+let opacity = 0;
+
+const fadeIn = setInterval(() => {
+    if (opacity >= 1) clearInterval(fadeIn);
+    opacity += 0.02;
+    document.body.style.opacity = opacity;
+}, 50);
